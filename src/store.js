9120c233929e3a548c17f00eac43c91c6a4fd26c
SivaSurender <e.surender14@gmail.com>
2023-09-17 19:20:12 +0530

    Added code

--- src/store.js
+++ src/store.js
@@ -25,7 +25,7 @@ function reducerFn(state = initialState, action) {
       return {
         ...state,
         loan: action.payload.amount,
-        loanPurpose: action.payload.loanPurpose,
+        loanPurpose: action.payload.purpose,
         balance: state.balance + action.payload.amount,
       };
     }
@@ -48,6 +48,8 @@ function reducerFn(state = initialState, action) {
 // dispatch the reqd action with store
 // call getState method on store to log current state
 
+// create action creators to automate dispatching actions
+
 const store = createStore(reducerFn);
 
 store.dispatch({ type: "account/deposit", payload: 7000 });
@@ -59,7 +61,7 @@ console.log(store.getState());
 
 store.dispatch({
   type: "account/requestLoan",
-  payload: { amount: 10000, loanPurpose: "buying house" },
+  payload: { amount: 10000, purpose: "buying house" },
 });
 console.log(store.getState());
 
@@ -67,4 +69,33 @@ store.dispatch({
   type: "account/payLoan",
   payload: 10000,
 });
+console.log(store.getState());
+
+// ACTION CREATORS
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+store.dispatch(deposit(7000));
+console.log(store.getState());
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+store.dispatch(withdraw(2000));
+console.log(store.getState());
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+store.dispatch(requestLoan(10000, "buying house"));
+console.log(store.getState());
+
+function payLoan(amount) {
+  return { type: "account/payLoan", payload: amount };
+}
+store.dispatch(payLoan(10000));
 console.log(store.getState());
