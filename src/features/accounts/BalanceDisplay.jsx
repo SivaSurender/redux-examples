0284a3f98690db1e1e6ee23a2611308f9e09ca0f
SivaSurender <e.surender14@gmail.com>
2023-09-24 10:53:45 +0530

    added code

--- src/features/accounts/BalanceDisplay.jsx
+++ src/features/accounts/BalanceDisplay.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { connect, useSelector } from "react-redux";
 
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
@@ -7,6 +7,7 @@ function formatCurrency(value) {
   }).format(value);
 }
 
+// hooks method
 function BalanceDisplay() {
   const balance = useSelector((store) => store.account.balance);
   console.log(balance);
@@ -14,3 +15,24 @@ function BalanceDisplay() {
 }
 
 export default BalanceDisplay;
+
+// connect method before hooks impl.
+
+/**
+ * 
+1. comp needs to be wired with connect 
+2. this connet taks in a function and this connect with arg takes in the component as arg
+3. this connect function takes state as arg and it shoul return an object
+4. and ths objectr reqd value should be parsed as a prop from the component
+ */
+// function BalanceDisplay({ balance }) {
+//   return <div className="balance">{formatCurrency(balance)}</div>;
+// }
+// function mapStateToProps(state) {
+//   console.log(state, "map");
+//   return {
+//     balance: state.account.balance,
+//   };
+// }
+
+// export default connect(mapStateToProps)(BalanceDisplay);
