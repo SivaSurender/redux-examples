bc86f3e0ee451f61868589416422188fa2f34d1d
SivaSurender <e.surender14@gmail.com>
2023-10-07 17:23:25 +0530

    added toolkit

--- src/store.js
+++ src/store.js
@@ -2,10 +2,21 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/CustomerSlice";
 import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
-const masterReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+// classsic redux
+// const masterReducer = combineReducers({
+//   account: accountReducer,
+//   customer: customerReducer,
+// });
+
+// export const store = createStore(masterReducer, applyMiddleware(thunk));
+
+// using redux toolkit
+
+export const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-export const store = createStore(masterReducer, applyMiddleware(thunk));
